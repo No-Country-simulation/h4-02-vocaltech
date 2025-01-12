4593ab72442b5b6d5e27ffefa31409a30846ea79
LumSeg <lucas_lms_24@hotmail.com>
2025-01-12 00:20:42 -0300

    config data base AirTable in microService-email

--- back/microServices-leads/src/server.ts
+++ back/microServices-leads/src/server.ts
@@ -2,11 +2,12 @@ import cors from "cors";
 import morgan from "morgan";
 import express from "express";
 
-
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 server.use(morgan("dev"))
+
+server.use("/api", );
 
 export default server;
--- back/microServices-whatsApp/src/index.ts
+++ back/microServices-whatsApp/src/index.ts
@@ -1,14 +1,10 @@
 import server from "./server";
-import "reflect-metadata";
 import dotenv from "dotenv";
-import { dataSource } from "./config/dbConfig";
 
 dotenv.config();
 
-dataSource.initialize()
-   .then( res => {
-      console.log("Database connected");
-      server.listen(process.env.PORT, () => {
-         console.log(`Server listening on port ${process.env.PORT}`);
-      });
-   })
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
