126df281596b1874acf850d05192eaeec523e405
alicia4079 <alicia4079@gmail.com>
2025-01-15 17:44:51 +0100

    feat navbar and routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
 import Home from './pages/home'
+import Entrepreneurs from './pages/entrepreneurs'
+import Companies from './pages/companies'
+import Login from './pages/login'
 
 function App() {
-
-
   return (
     <>
-      <Home />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/emprendedores' element={<Entrepreneurs />} />
+        <Route path='/empresas' element={<Companies />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </>
   )
 }
