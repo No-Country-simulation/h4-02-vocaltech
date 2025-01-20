73f7629c0e9bf427eb74ac84b16e04740ceead78
LumSeg <lucas_lms_24@hotmail.com>
2025-01-20 02:30:45 -0300

    ' '

--- back/src/utils/airtableInterfaces.ts
+++ back/src/utils/airtableInterfaces.ts
@@ -1,17 +1,17 @@
-export interface AirtableRecord {
-    id: string;
-    fields: {
-      name: string;
-      email: string;
-      password: string;
-      phone: string;
-      company: string;
-      [key: string]: any; // Permite otros campos dinámicos si es necesario
-    };
+export interface AirtableRecordUser {
+  id: string;
+  fields: {
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    company: string;
+    [key: string]: any; // Permite otros campos dinámicos si es necesario
+  };
 }
   
 export interface AirtableResponse {
-    records: AirtableRecord[];
+  records: AirtableRecordUser[];
 }
   
   
