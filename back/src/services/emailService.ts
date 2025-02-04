e28a850ba7adb069b0d666683be9e7d09573653f
LumSeg <lucas_lms_24@hotmail.com>
2025-02-04 16:22:36 -0300

    ' '

--- back/src/services/emailService.ts
+++ back/src/services/emailService.ts
@@ -16,26 +16,3 @@ export const emailService = {
     await sgMail.send(msg);
   },
 };
-
-export const emailDiagnoticService = {
-
-  async sendWelcomeEmail(to: string, name: string) {
-    const msg = {
-      to,
-      from: 'lms.segovia86@gmail.com', // Email verificado en SendGrid
-      subject: "Hola ${name}",
-      text: `Hola ${name}, Gracias por completar nuestro diagnóstico en VocalTech. Hemos recibido tu información y estamos analizando los mejores recursos para ayudarte a potenciar tu comunicación y liderazgo.`,
-      html: `<h1>Hola ${name}!</h1><p>Gracias por completar nuestro diagnóstico en VocalTech. Hemos recibido tu información y estamos analizando los mejores recursos para ayudarte a potenciar tu comunicación y liderazgo.</p> <br><br>
-      <p>En breve, recibirás una recomendación personalizada con las mejores soluciones para ti.</p>
-      <br><br>
-      <p>¡Nos emociona acompañarte en este camino!</p>
-        <br><p>Saludos, 
-        <br>El equipo de VocalTech
-      </p>
-      `,
-    };
-
-    await sgMail.send(msg);
-  },
-
-}     
