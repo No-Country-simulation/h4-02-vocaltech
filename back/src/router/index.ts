7d783a9b7837a550ff3c69a2e0031567688face4
LumSeg <lucas_lms_24@hotmail.com>
2025-02-11 06:18:49 -0300

    Add endpoint to send custom email with user input

--- back/src/router/index.ts
+++ back/src/router/index.ts
@@ -7,6 +7,8 @@ import ProductRouter from "./productRoutes";
 import LeadRouter from "./leadRoutes";
 import WappRouter from "./wappRoutes";
 import ChatRouter from "./chatRoutes";
+import emailRouter from "./emailRoutes";
+
 const router = Router();
 
 // Rutas para AirTable
@@ -18,6 +20,7 @@ router.use("/products", ProductRouter);
 router.use("/leads", LeadRouter);
 router.use("/chats", ChatRouter);
 router.use("/wapps", WappRouter);
+router.use("/emails", emailRouter);
 /*
 router.use("/notifications");
 router.use("/admin",);
