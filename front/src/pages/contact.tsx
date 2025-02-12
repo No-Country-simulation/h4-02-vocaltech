a359691d042172422d1b2589b2ae0745597eadda
alicia4079 <alicia4079@gmail.com>
2025-02-13 00:02:38 +0100

    fix error

--- front/src/pages/contact.tsx
+++ front/src/pages/contact.tsx
@@ -9,9 +9,9 @@ const Contact = () => {
           ¿Tenés dudas o querés más información?
         </h2>
         <p className='mt-7 text-lg font-semibold mb-6 text-primary_400'>
-          Déjanos un mensaje{' '}
+          Déjanos un{' '}
           <a href='https://h4-02-vocaltech.vercel.app/wapps' target='_blank'>
-            mensaje
+            mensaje{' '}
           </a>
           y te responderemos lo antes posible.
         </p>
